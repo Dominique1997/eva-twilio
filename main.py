9fb834af5505af9dd44d5ea6a4df8c11cd945353
Dominique1997 <dominiqueliebens@telenet.be>
2024-05-03 19:49:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ auth_token  = "AUTH_TOKEN"
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+32479407776",
+    to="receving_phone_number",
     from_="+12566009835",
     body="Hello eva here!")
 
